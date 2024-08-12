db7b74605787c8c12fe62d3f0cc8ff66dee18842
melissaceee <cheng.melissa@outlook.com>
2024-08-12 14:36:25 -0400

    update 5

--- models/user.js
+++ models/user.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose');
 const foodSchema = new mongoose.Schema({
   name: { type: String, required: true }
 })
+
 const userSchema = mongoose.Schema({
   username: {
     type: String,
