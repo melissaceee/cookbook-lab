89092565d7ab6ae1c0339a36e8450213c8dd6da1
melissaceee <cheng.melissa@outlook.com>
2024-08-12 12:33:29 -0400

    middleware update

--- controllers/auth.js
+++ controllers/auth.js
@@ -24,4 +24,51 @@ router.post('/sign-up', async (req, res) => {
       return res.send('Username already taken.');
     }
 
- 
+    // Ensure passwords match
+    if (req.body.password !== req.body.confirmPassword) {
+        return res.send('Password and Confirm Password must match');
+      }
+  
+      // Hash the password
+      const hashedPassword = await bcrypt.hash(req.body.password, 10);
+      req.body.password = hashedPassword;
+  
+      // Create the new user
+      await User.create(req.body);
+  
+      res.redirect('/auth/sign-in');
+    } catch (error) {
+      console.log(error);
+      res.redirect('/');
+    }
+  });
+  
+  router.post('/sign-in', async (req, res) => {
+    try {
+      // Get user from database
+      const userInDatabase = await User.findOne({ username: req.body.username });
+      if (!userInDatabase) {
+        return res.send('Login failed. Please try again.');
+      }
+  
+      // Test password with bcrypt
+      const validPassword = await bcrypt.compare(req.body.password, userInDatabase.password);
+      if (!validPassword) {
+        return res.send('Login failed. Please try again.');
+      }
+  
+      // Create session
+      req.session.user = {
+        username: userInDatabase.username,
+        _id: userInDatabase._id
+      };
+  
+      res.redirect('/');
+    } catch (error) {
+      console.log(error);
+      res.redirect('/');
+    }
+  });
+  
+  module.exports = router;
+  
--- server.js
+++ server.js
@@ -22,6 +22,45 @@ mongoose.connection.on('connected', () => {
     console.log(`Connected to MongoDB ${mongoose.connection.name}.`);
   });
 
+  app.use(morgan('tiny'))
+  app.use(express.urlencoded({ extended: false }));
+  app.use(methodOverride('_method'));
+  // app.use(morgan('dev'));
+  app.use(
+    session({
+      secret: process.env.SESSION_SECRET,
+      resave: false,
+      saveUninitialized: true,
+    })
+  );
+  app.get('/', (req, res) => {
+    res.render('index.ejs', {
+      user: req.session.user,
+    });
+  });
+  
+  app.get('/vip-lounge', (req, res) => {
+    if (req.session.user) {
+      res.send(`Welcome to the party ${req.session.user.username}.`);
+    } else {
+      res.send('Sorry, no guests allowed.');
+    }
+  });
+
+  
+  app.use(passUserToView)
+  app.use('/auth', authController);
+  app.use(isSignedIn)
+  app.use('/users/:userId/foods', foodsController)
+  app.use('/users', usersController)
+  
+  
+  app.listen(port, () => {
+    console.log(`The express app is ready on port ${port}!`);
+  });
+
+
+
 
 app.listen(port, () => {
     console.log(`The express app is ready on port ${port}!`);
